ffe9143464decfe8febe7cbe9b44cd80df52ca07
evertcolombia <evertescalante@gmail.com>
2019-09-19 16:26:30 +0000

    C progam that contains exactly a string follow by a new line hotfix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/**
+/*
  * main - is the entry point
  *
  * Return: Always 0
@@ -8,6 +8,6 @@
 
 int main (void) 
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return (0);
 }

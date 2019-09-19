6da34189a4de489360a5c58b262411ee88b07843
evertcolombia <evertescalante@gmail.com>
2019-09-19 16:19:33 +0000

    C progam that contains exactly a string follow by a new line

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - is the entry point
+ *
+ * Return: Always 0
+ */
+
+int main (void) 
+{
+	puts("\"Programming is like building a multilinguak puzzle");
+	return (0);
+}

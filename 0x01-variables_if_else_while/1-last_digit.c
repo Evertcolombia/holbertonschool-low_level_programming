29401c3d7f3cc28c60158c968eeef392ca865512
evertcolombia <evertescalante@gmail.com>
2019-09-19 22:01:39 +0000

    hotfix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,23 +11,23 @@
  */
 int main(void)
 {
-	int n, last;
+int n, last;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	last = n % 10;
-	if ((last < 6) && (last != 0))
-        {
-        printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
-        }
-	if (last > 5)
-	{
-	printf("Last digit of %d is %d and is greater than 5\n", n, last);
-	}
-	if (last == 0)
-	{
-	printf("Last digit of %d is %d and is 0\n", n, last);
-	}
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+last = n % 10;
+if ((last < 6) && (last != 0))
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
+}
+if (last > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, last);
+}
+if (last == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, last);
+}
+return (0);
 }

b5ec83406a8645616febe05196b8b1e2e628a804
evertcolombia <evertescalante@gmail.com>
2019-09-19 21:57:34 +0000

    hotfix

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,15 +6,13 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) 
+int main(void)
 {
 	int ch;
-	
+
 	for (ch = 'a'; ch <= 'z'; ch++)
-	{
-		putchar(ch);
-	}
+	putchar(ch);
+
 	putchar('\n');
 	return (0);
-
 }

12e31a9659c19fe3926a4509b1a57e0ebc0478a1
evertcolombia <evertescalante@gmail.com>
2019-09-19 17:08:04 +0000

    program that prints exactly a string with printf hotfix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
 * main - Entry point
 *
 * Return always 0 (success)
 */
-
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
-};
+}

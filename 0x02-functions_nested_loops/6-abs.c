7b8a67f1b4b716a47dc3988739fbf36d67580130
evertcolombia <evertescalante@gmail.com>
2019-09-25 18:56:21 +0000

    function that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+
+/**
+ * _bs - show an int by stdout c
+ * @n: The character to print
+ *
+ * Return: On (success) 1
+ */
+
+
+int _abs(int n)
+{
+	int res;
+	
+	res = abs(n);
+	return(res);	
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -12,4 +12,7 @@ int _islower(int c);
 int _isalpha(int c);
 
 int print_sign(int n);
+
+int _abs(int);
+
 #endif

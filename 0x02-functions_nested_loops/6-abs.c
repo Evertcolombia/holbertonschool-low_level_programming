175e39448a472782fba6d64508fe6086eb89a271
evertcolombia <evertescalante@gmail.com>
2019-09-25 19:02:10 +0000

    function that computes the absolute value of an integer hotfix again

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 
 /**
- * _bs - show an int by stdout c
+ * _abs - show an int by stdout c
  * @n: The character to print
  *
  * Return: On (success) 1
  */
 
-int _abs (int n)
+int _abs(int n)
 {
 	int res;
 

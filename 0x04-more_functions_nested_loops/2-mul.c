6c95e6486cec1650e93d1cec4a41ad060084737c
evertcolombia <evertescalante@gmail.com>
2019-09-26 15:55:31 +0000

    function that multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isupper - verificate if is upper the parameter
+ *@c: int with a character
+ *
+ * Return: 1 on success
+ */
+
+int mul(int a, int b)
+{
+	int mult;
+	
+	mult = a * b;
+	return (mult);
+}

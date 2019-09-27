c86a4dc1ef736c10267232e72f90fe9ded9b5055
evertcolombia <evertescalante@gmail.com>
2019-09-27 16:41:51 +0000

    hotfix

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Main - Entry Point
+ * main - Entry Point
  *
  * Return: Always 0 (success)
  */
@@ -9,26 +9,25 @@
 int main(void)
 {
 
-int i = 48;
-int j = 48;
+	int i = 48;
+	int j = 48;
 
-while (i < 58)
-{
-while (j < 58)
-{
-putchar(i);
-putchar(j);
-if (i != 57 || j != 57)
-{
-putchar(',');
-putchar(' ');
-}
-j++;
-}
-j = 48;
-i++;
-}
-putchar('\n');
-return (0);
-
+	while (i < 58)
+	{
+		while (j < 58)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 57 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		j = 48;
+		i++;
+	}
+	putchar('\n');
+	return (0);
 }

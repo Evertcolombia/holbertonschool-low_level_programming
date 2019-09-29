f7507caa928be573e8ec211cedebd1e8c4d9866c
evertcolombia <evertescalante@gmail.com>
2019-09-29 18:29:16 +0000

    hotfix

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - Entry Point
+ *
+ */
+
+int main(void)
+{
+
+	int a, b;
+
+	for (a = 0; a < 9; a++)
+	{
+		for (b = 1; b < 10; b++)
+		{
+			if (b > a)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

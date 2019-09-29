02677fe384841575d9c98abbfa8fc2885ccfca73
evertcolombia <evertescalante@gmail.com>
2019-09-29 20:03:03 +0000

    hotfix betty

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *
  * main - Entry Point
  *
+ * Return - always 0 (success)
  */
 int main(void)
 {
@@ -13,16 +13,13 @@ int main(void)
 	for (a = 0; a < 9; a++)
 	{
 		for (b = 1; b < 10; b++)
-		{			
+		{
 			if (b > a)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
-				if (b != 8 || a != 9)
-				{
-					putchar(44);
-					putchar(32);
-				}
+				putchar(44);
+				putchar(32);
 			}
 		}
 	}

e46da0c792248267fe9ee0d83bf166f78f7a2e1a
evertcolombia <evertescalante@gmail.com>
2019-09-30 02:45:47 +0000

    hotfix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,38 +1,38 @@
 #include "holberton.h"
 
 /**
- * jack_bauer - entry point
+ * jack_bauer - Hour times
  *
- * Return - Absolute
+ * Return - absolute.
  */
 void jack_bauer(void)
 {
 	int h1, h2, m1, m2;
 
-	for (h1 = 48; h1 < 51; h1++)
+	for (h1 = 0; h1 <= 2; h1++)
 	{
-		for (h2 = 48; h2 <= 57; h2++)
+		for (h2 = 0; h2 <= 9; h2++)
 		{
-			for (m1 = 48; m1 < 54; m1++)
+			for (m1 = 0; m1 < 6; m1++)
 			{
-				for (m2 = 48; m2 <= 57; m2++)
+				for (m2 = 0; m2 <= 9; m2++)
 				{
-					if (m1 <= 1)
+					if (h1 <= 1)
 					{
-						_putchar(h1);
-						_putchar(h2);
+						_putchar(h1 + '0');
+						_putchar(h2 + '0');
 						_putchar(58);
-						_putchar(m1);
-						_putchar(m2);
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
 						_putchar('\n');
 					}
-					if ((h1 == 50) && (h2 < 52))
+					if ((h1 == 2) && (h2 < 4))
 					{
-						_putchar(h1);
-						_putchar(h2);
+						_putchar(h1 + '0');
+						_putchar(h2 + '0');
 						_putchar(58);
-						_putchar(m1);
-						_putchar(m2);
+						_putchar(m1 + '0');
+						_putchar(m2 + '0');
 						_putchar('\n');
 					}
 				}

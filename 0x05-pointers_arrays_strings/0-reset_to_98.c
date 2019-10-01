6c543f30daf95296424cce4b746dda5116453c28
evertcolombia <evertescalante@gmail.com>
2019-10-01 16:03:31 +0000

    0- hotfix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,15 +1,10 @@
-#include "holberton.h"
-
 /**
- * reset_to_98  - Entry point
+ * reset_to_98  - set the integer to 98
+ * @n: a pointer the intenger we want to set to 98
  *
  * Return: none
  */
 void reset_to_98(int *n)
 {
-	int *ptr;
-
-	ptr = &*n;
-
-	*ptr = 98;
+	*n = 98;
 }

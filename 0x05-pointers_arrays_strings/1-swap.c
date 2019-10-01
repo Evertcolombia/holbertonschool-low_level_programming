824f3feef05be7d0e5fb0535a7bbb08dba481a13
evertcolombia <evertescalante@gmail.com>
2019-10-01 16:10:33 +0000

    hotfiz 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,3 +1,10 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: a pointer the intenger we want to set to 42
+ * @b: pointer the intenger we want to set to 98
+ *
+ * Return: none
+ */
 void swap_int(int *a, int *b)
 {
 	*a = 42;

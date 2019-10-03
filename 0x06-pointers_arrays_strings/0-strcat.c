149e53d7d500d3fe5eea5463e0c109812f03bef6
evertcolombia <evertescalante@gmail.com>
2019-10-03 18:24:42 +0000

    unction that concatenates two strings.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,25 +1,29 @@
 #include "holberton.h"
 
+/**
+ * *_strcat - concatenate dos strings
+ * @dest: pointer to a string
+ * @src: pointer to the strign to concatenate
+ *
+ * Return: dest
+ */
+
 char *_strcat(char *dest, char *src)
 {
-	int length; 
+	int length;
 	int count = 0;
 	int a = 0;
 
-	while(dest[a])
+	while (dest[a])
 		++a;
 	length = a;
 
-	while(count <= length && src[count] != '\0')
+	while (count <= length && src[count] != '\0')
 	{
 		dest[length + count] = src[count];
 		count++;
-		
 	}
-	/*for(count = 0; count <= length && src[count] != '\0'; count++)
-		dest[length + count] = src[count];*/
 	dest[length + count] = '\0';
 
 	return (dest);
-
 }

cd2e8a5012d37f2b6232c41373f7dbdf4ae3fc79
evertcolombia <evertescalante@gmail.com>
2019-10-04 00:36:16 +0000

    hotfix

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+void reverse_array(int *a, int n)
+{
+    int b [1000];
+    int count;
+    int c = 0;
+    int lenth;
+
+    length = n -1
+
+    for(count = length; count >=  0 ; count--)
+    {
+        b[c] = a[count];
+        c++; :
+    }
+    c = 0;
+    for (; c <= length; c++)
+        a[c] = num[c]
+}

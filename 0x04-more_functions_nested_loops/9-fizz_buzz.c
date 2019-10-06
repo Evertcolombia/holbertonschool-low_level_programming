bcc686f7c4bf05ef064ad45d5008eae1214894e2
evertcolombia <evertescalante@gmail.com>
2019-10-06 00:55:47 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Al ways 0 (success)
+ *
+ */
+int main(void)
+{
+	int count;
+
+	for (count = 1; count < 100; count++)
+	{
+		if (count % 3 == 0 && count % 5 == 0)
+		{
+			printf("FizzBuzz");
+			putchar(32);
+		}
+		else if (count % 3 == 0)
+		{
+			printf("Fizz");
+			putchar(32);
+		}
+		else if (count % 5 == 0)
+		{
+			printf("Buzz");
+			putchar(32);
+		}
+		else
+		{
+			printf("%d", count);
+			putchar(32);
+		}
+	}
+	return (0);
+}

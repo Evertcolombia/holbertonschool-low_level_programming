c080391ec47ff50afad5d954b0b165347b5e559e
evertcolombia <evertescalante@gmail.com>
2019-10-07 04:56:03 +0000

    hotfix -4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,20 +1,24 @@
 #include "holberton.h"
 
+/**
+ * reverse_array - reverse
+ * @a: value
+ * @n: value
+ * Return: none
+ */
 void reverse_array(int *a, int n)
 {
-    int b [1000];
-    int count;
-    int c = 0;
-    int length;
+	int b[1000];
+	int count, length;
+	int c = 0;
 
-    length = n - 1;
-
-    for(count = length; count >=  0; count--)
-    {
-        b[c] = a[count];
-        c++;
-    }
-    c = 0;
-    for (; c <= length; c++)
-        a[c] = b[c];
+	length = n - 1;
+	for (count = length; count >=  0; count--)
+	{
+		b[c] = a[count];
+		c++;
+	}
+	c = 0;
+	for (; c <= length; c++)
+		a[c] = b[c];
 }

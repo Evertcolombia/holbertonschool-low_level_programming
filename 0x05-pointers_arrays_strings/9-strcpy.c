967bf894125107f0cb63e7204e581259b8f4af49
evertcolombia <evertescalante@gmail.com>
2019-10-07 04:45:36 +0000

    hotfix 9-

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copy a string from another pointer
+ * @dest: pointer to a string
+ * @src: pointer to the strign to concatenate
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+        int count = 0;
+	int length;
+	while (src[count])
+		++count;
+	length = count;
+	count = 0;
+      /*  while (count < length  && src[count] != '\0')*/
+	while (count <= length)
+        {
+                dest[count] = src[count];
+                count++;
+        }
+       /* while (count < length)
+        {
+                dest[count] = '\0';
+                count++;
+        }*/
+        return (dest);
+}
+

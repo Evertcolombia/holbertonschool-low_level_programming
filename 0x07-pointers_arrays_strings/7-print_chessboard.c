f5773384396fd8987362ab68218243eb8eab51d8
evertcolombia <evertescalante@gmail.com>
2019-10-08 04:55:08 +0000

    hotfix

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+void print_chessboard(char (*a)[8])
+{
+	int count, c;
+
+	for (c = 0; c < 8; c++)
+	{
+		for (count = 0; count < 8; count++)
+			_putchar(a[c][count]);
+		_putchar(10);
+	}
+}
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -3,3 +3,6 @@ char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);

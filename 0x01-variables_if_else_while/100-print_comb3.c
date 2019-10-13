5a1d9de5626830f5f09adc401026d32b758ed633
evertcolombia <evertescalante@gmail.com>
2019-10-13 03:37:17 +0000

    hotfix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,26 +3,29 @@
 /**
  * main - Entry Point
  *
- * Return - always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
+	int num1, num2 = 49, num3;
 
-	int a, b;
-
-	for (a = 0; a < 9; a++)
+	for (num1 = 48; num1 < 57; num1++)
 	{
-		for (b = 1; b < 10; b++)
+		num3 = num2;
+		for (; num2 <= 57; num2++)
 		{
-			if (b > a)
+			putchar(num1);
+			putchar(num2);
+			if (num1 == 56 && num2 == 57)
+				putchar(10);
+			else
 			{
-				putchar(a + '0');
-				putchar(b + '0');
 				putchar(44);
 				putchar(32);
 			}
 		}
+		num2 = num3;
+		num2++;
 	}
-	putchar('\n');
 	return (0);
 }

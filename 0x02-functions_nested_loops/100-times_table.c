80a5c68dc176c844a3489dcb4f6565ec34d3b5e9
evertcolombia <evertescalante@gmail.com>
2019-10-14 05:38:06 +0000

    hotfix

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,34 +1,46 @@
 #include "holberton.h"
 
+/**
+ * print_times_table - prints tables times
+ * @n: int to number
+ *
+ * return: none
+ */
+
 void print_times_table(int n)
 {
 	int mul, count, res;
 
-	for (mul = 0; mul <= n; mul++)
+	if (n <= 15 && n >= 0)
 	{
-		for (count = 0; count <= n; count++)
+		for (mul = 0; mul <= n; mul++)
 		{
-			res = mul * count;
-			if (res > 9)
+			for (count = 0; count <= n; count++)
 			{
-				_putchar(32);
-				_putchar(res / 10 + '0');
-				_putchar(res % 10 + '0');
-			}
-			else
-			{
-				if (count != 0)
+				res = mul * count;
+				if (res > 9)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(32);
+					if (res > 99)
+					{
+						_putchar(32), _putchar((res / 10) / 10 + '0');
+						_putchar((res % 100) / 10 + '0'), _putchar(res % 10 + '0');
+					}
+					else
+					{
+						_putchar(32), _putchar(32);
+						_putchar(res / 10 + '0'), _putchar(res % 10 + '0');
+					}
 				}
-				_putchar(res + '0');
+				else
+				{
+					if (count != 0)
+						_putchar(32), _putchar(32), _putchar(32);
+					_putchar(res + '0');
+				}
+				if (count != n)
+					_putchar(44);
 			}
-			if (count != 9)
-				_putchar(44);
-				
+			_putchar(10);
 		}
-		_putchar('\n');
 	}
 }

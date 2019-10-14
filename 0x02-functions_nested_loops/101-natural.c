ba08724cc25ae068629b34b42ef4eb55c0f627bb
evertcolombia <evertescalante@gmail.com>
2019-10-14 17:09:13 +0000

    hotfix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -11,11 +11,13 @@ int main(void)
 
 	for (num = 1; num < 1024; num++)
 	{
-		if (num % 3 == 0 || num % 5 == 0)
+		if (num % 3 == 0)
 			mul3 = mul3 + num;
-		mul3 = mul3;
+		if (num % 5 == 0)
+			mul5 = mul5 + num;
+		mul3 = mul3, mul5 = mul5;
 	}
-	total = mul3;
+	total = mul3 + mul5;
 	printf("%d", total), putchar(10);
 	return (0);
 }

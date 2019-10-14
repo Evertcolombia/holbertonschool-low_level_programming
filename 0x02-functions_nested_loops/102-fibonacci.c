7953bf5018f16dbcd1f273debab00863f5ea1204
evertcolombia <evertescalante@gmail.com>
2019-10-14 21:10:13 +0000

    hotfix

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main (void) 
+{
+	unsigned long f = 0, f1 = 1, res; 
+	int count;
+
+	for (count = 0; count <= 50; count++)
+	{
+		res = f + f1;
+		f = f1, f1 = res;
+
+		if (res > 9)
+		{
+			if (res > 99)
+				printf("%lu", res);
+			else
+				printf("%lu", res);
+		}
+		else
+			printf("%lu", res);
+		if (count != 50)
+
+			putchar(44);
+			putchar(32);
+	}
+	putchar(10);
+	return (0);
+}

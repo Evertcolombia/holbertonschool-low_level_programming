de9722616f2e2fb723fb56d64876d4c68d69867f
evertcolombia <evertescalante@gmail.com>
2019-10-15 01:05:41 +0000

    hotfix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,7 @@
  */
 int main(void)
 {
-	char phrase[];
-
-	phrase = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char phrase[]  = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", sizeof(phrase));
 	return (1);

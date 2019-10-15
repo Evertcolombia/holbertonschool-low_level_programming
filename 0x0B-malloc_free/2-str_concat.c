3b0d8cc07d25c69e2b361f373463de9a7bf495d7
evertcolombia <evertescalante@gmail.com>
2019-10-15 22:48:10 +0000

    hotfix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,8 +12,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, z = 0, h = 0, count, length, length1, lengthto;
 	char *str;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	while (s1[i])
 		i++;

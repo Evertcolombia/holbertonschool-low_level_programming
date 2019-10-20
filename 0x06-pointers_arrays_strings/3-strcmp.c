ffd0f583138e18c1efc49632b9458109ca015f86
evertcolombia <evertescalante@gmail.com>
2019-10-20 01:56:39 +0000

    hotfix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,7 +6,8 @@
  * @s2: string 2
  * Return: value
  */
-int _strcmp(char *s1, char *s2)
+
+/*int _strcmp(char *s1, char *s2)
 {
 	int count = 0;
 	int value = 0;
@@ -24,4 +25,33 @@ int _strcmp(char *s1, char *s2)
 		count++;
 	}
 	return (value);
+}*/
+
+/**
+ * _strlen - show the lenght of a string
+ * @s: pointe to  pass the array
+ *
+ * Return: none
+ */
+int _strlen(char *s)
+{
+    int i = 0;
+
+    while (s[i])
+        ++i;
+    return (i);
+}
+
+int _strcmp(char *s1, char *s2)
+{
+    int a, b;
+
+    a = _strlen(s1), b = _strlen(s2);
+
+    if (a > b)
+        return (15);    
+    else if (b > a)
+        return (-15);
+    else
+    return(0);
 }

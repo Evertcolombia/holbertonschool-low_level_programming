1e61968a349a45db9bc224d27987fa9c0673cece
evertcolombia <evertescalante@gmail.com>
2019-10-21 21:13:11 +0000

    hotfix

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+This directory contains exercices wirh structures in 'C' 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG
+#define DOG
+/**
+ * struct dog - Structure
+ * @name: value
+ * @age: float value
+ * @owner: pointer to value
+ *
+ * Description: structure for a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+	/**
+	 * @name: pointer to a value
+	 * @age: value with an float
+	 * @owner: pointer to a value
+	 *
+	 * Description: This structure is for a class dog
+	 */
+};
+#endif /* DOG */

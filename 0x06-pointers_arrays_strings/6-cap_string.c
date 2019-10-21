ff0b6f68c686bd5d9b4c05604313bc411b15742e
evertcolombia <evertescalante@gmail.com>
2019-10-21 03:16:01 +0000

    hotfix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,33 +1,28 @@
+/**
+ * cap_string - capitalice all words in a string
+ * @str: string
+ *
+ * Return: string
+ */
 char *cap_string(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    while (str[i])
-    {
-        if (str[i] == 9)
-                str[i] = (str[i] + 23);
-      
-        else if (str[i] == 10)
-        {
-            i++;
-            if (str[i] >= 97 && str[i]<= 122)
-                str[i] = (str[i] - 32);
-            i--;
-        }
+	while (str[i])
+	{
+		if ((str[i] == 44) || (str[i] == 59) ||
+		(str[i] == 32) || (str[i] == 33) ||
+		(str[i] == 63) || (str[i] == 34) ||
+		(str[i] == 40) || (str[i] == 41) ||
+		(str[i] == 123) || (str[i] == 125) ||
+		(str[i] == 46) || (str[i] == 10))
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] = (str[i + 1] - 32);
+		}
 
-        else if (str[i] ==  46 && str[i + 1] >= 97 && str[i + 1] <= 122)
-        {
-            i++;
-            str[i] = (str[i] - 32);
-            i--;
-        }
-        else if (str[i] == 32)
-        {
-            i++;
-            if (str[i] >= 97 && str[i]<= 122)
-                str[i] = (str[i] - 32);
-            i--;
-        }
-        i++;    
-    } return (str);   
+		else if (str[i] == 9)
+			str[i] = (str[i] + 23);
+		i++;
+	} return (str);
 }

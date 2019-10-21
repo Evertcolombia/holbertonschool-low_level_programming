e603a4b283138fa06196c64e748cd738d6898d7e
evertcolombia <evertescalante@gmail.com>
2019-10-21 23:00:05 +0000

    hotfix

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,6 +2,8 @@
 #include "dog.h"
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+	{}
 	if (d != NULL)
 	{
 		if (d->name  == NULL)
@@ -17,6 +19,4 @@ void print_dog(struct dog *d)
 		else
 			printf("Owner: %s\n", d->owner);
 	}
-	else
-	{}
 }

f6e1ab64139100422cf03a463375302102c1294b
evertcolombia <evertescalante@gmail.com>
2019-10-21 03:50:23 +0000

    hotfix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,12 @@ char *cap_string(char *str)
 
 	while (str[i])
 	{
-		if ((str[i] == 44) || (str[i] == 59) ||
-		(str[i] == 32) || (str[i] == 33) ||
-		(str[i] == 63) || (str[i] == 34) ||
-		(str[i] == 40) || (str[i] == 41) ||
-		(str[i] == 123) || (str[i] == 125) ||
-		(str[i] == 46) || (str[i] == 10))
+		if ((str[i] == ',') || (str[i] == ';') ||
+		(str[i] == '!') || (str[i] == '.') ||
+		(str[i] == '?') || (str[i] == '"') ||
+		(str[i] == '(') || (str[i] == ')') ||
+		(str[i] == '{') || (str[i] == '}') ||
+		(str[i] == '\n') || (str[i] == ' '))
 		{
 			i++;
 			if (str[i] >= 97 && str[i] <= 122)
@@ -23,7 +23,7 @@ char *cap_string(char *str)
 			i--;
 		}
 
-		else if (str[i] == 9)
+		else if (str[i] == '\t')
 		{
 			str[i] = (str[i] + 23);
 			i++;

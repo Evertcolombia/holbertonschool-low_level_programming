4e125b6e44800c3b9dd3d0cbc4c9e48160b78e4d
evertcolombia <evertescalante@gmail.com>
2019-10-22 03:28:04 +0000

    hotfix

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,52 +9,68 @@
  */
 
 int _strlen(char *s)
-
 {
 	int a = 0;
 
 	while (s[a])
 		++a;
-
 	return (a);
 }
 
 /**
- * *new_dog - create a new intance
- *@name: string
- *@age: value
- *@owner: value
+ * *_strcpy - copy a string from another pointer
+ * @dest: pointer to a string
+ * @src: pointer to the strign to concatenate
  *
- * Return: value
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0, length;
+
+	length = _strlen(src);
+	while (count <= length)
+	{
+		dest[count] = src[count], count++;
+	}
+	return (dest);
+}
+
+/**
+ * *new_dog - new dog
+ * @name: string
+ * @age: value
+ * @owner: string
+ * Return: new  struct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t  *p;
+	dog_t *p;
 	int a, b;
+
+	a = _strlen(name), b = _strlen(owner);
 
 	p = malloc(sizeof(dog_t));
 	if (p == NULL)
 		return (NULL);
-	p->age = age;
-	p->name = name;
-	p->owner = owner;
 
-	a = _strlen(name);
-	b = _strlen(owner);
-
-	p->name = malloc(sizeof(char) * a + 1);
+	p->name = malloc(a + 1);
 	if (p->name == NULL)
 	{
 		free(p);
 		return (NULL);
 	}
+	p->name = _strcpy(p->name, name);
 
-	p->owner = malloc(sizeof(char) * b + 1);
+	p->owner = malloc(b + 1);
 	if (p->owner == NULL)
 	{
 		free(p->name);
 		free(p);
 		return (NULL);
 	}
+	p->owner = _strcpy(p->owner, owner);
+
+	p->age = age;
 	return (p);
 }

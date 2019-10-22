e763a3508643697d7c51c02ef848d5c5e80cf878
evertcolombia <evertescalante@gmail.com>
2019-10-22 03:48:39 +0000

    hotfix

---
+++ 0x0E-structures_typedef/101-preprocessor_abuse.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define E "Hello, Holberton"
+
+/**
+ * main - Entry point
+ */
+void main(void)
+{
+	printf("%s\n", E);
+}
--- 0x0E-structures_typedef/2-main.c
+++ 0x0E-structures_typedef/2-main.c
@@ -1,18 +1,11 @@
 #include <stdio.h>
-#include "dog.h"
-
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * main - Entry - point
+ * Return: 0 always
  */
+
 int main(void)
 {
-    struct dog my_dog;
-
-    my_dog.name = "Django";
-    my_dog.age = 3.5;
-    my_dog.owner = "Jay";
-    print_dog(&my_dog);
-    return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }

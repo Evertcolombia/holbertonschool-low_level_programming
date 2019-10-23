3a95dfc6138ae77f2339cb0ca5339b7f511e6789
evertcolombia <evertescalante@gmail.com>
2019-10-23 21:41:54 +0000

    hotfix

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * print_name - print a name value
+ * @name: string
+ * @f: function pinter
+ * 
+ * Return: none
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}

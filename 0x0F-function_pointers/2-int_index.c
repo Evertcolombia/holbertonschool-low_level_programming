04b0b5316a16d9165fd778c2599f4e7352357a83
evertcolombia <evertescalante@gmail.com>
2019-10-23 22:54:34 +0000

    hotfix

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,21 @@
+/**
+ * int_index - search for an element
+ * @array: array integers
+ * @size: value
+ * @cmp: pointer function
+ * 
+ * Return: 
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if(cmp(array[i]))
+			return(i);
+	}
+	return (-1);
+}

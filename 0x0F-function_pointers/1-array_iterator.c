c02bc39deaaa3b8f2d738ae6bba9c696d87a375e
evertcolombia <evertescalante@gmail.com>
2019-10-23 22:07:34 +0000

    hotfix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,10 @@
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
-
-	for (i = 0; i < size; i++)
-		action(array[i]);
+	
+	if (array != NULL && action != NULL)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
 }

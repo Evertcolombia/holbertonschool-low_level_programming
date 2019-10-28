81a252582a5c4d0dfb0ecdcad84dddada7869afa
evertcolombia <evertescalante@gmail.com>
2019-10-28 05:05:11 +0000

    hotfix

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,96 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * print_all - Prints all operation
+  * @format: The conversion specifier to prints
+  *
+  * Return: None
+  */
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	f_dt form_t[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_char_ptr}
+	};
+
+	unsigned int i = 0, j;
+	char *separator = "";
+
+	va_start(ap, format);
+
+	while (format != NULL && format[i])
+	{
+		for (j = 0; j < 4; j++)
+		{
+			if (format[i] == *form_t[j].type)
+			{
+				form_t[j].f(separator, ap);
+				separator = ", ";
+			}
+		}
+		i++;
+	}
+	va_end(ap);
+	putchar('\n');
+}
+
+/**
+  * print_char - Prints a char
+  * @separator: string pointer
+  * @ap: A list of variadic arguments
+  *
+  * Return: None
+  */
+void print_char(char *separator, va_list ap)
+{
+	printf("%s%c", separator, va_arg(ap, int));
+}
+
+/**
+  * print_int - Prints a integer
+  * @separator: string pointer
+  * @ap: A list of variadic argument
+  *
+  * Return: None
+  */
+void print_int(char *separator, va_list ap)
+{
+	printf("%s%i", separator, va_arg(ap, int));
+}
+
+/**
+  * print_float - Prints a float character
+  * @separator: string pointer
+  * @ap: A list of variadic argument
+  *
+  * Return: None
+  */
+void print_float(char *separator, va_list ap)
+{
+	printf("%s%f", separator, va_arg(ap, double));
+}
+
+/**
+  * print_char_ptr - Prints a char pointer content
+  * @separator: string pointer
+  * @ap: A list of variadic argument
+  *
+  * Return: None
+  */
+void print_char_ptr(char *separator, va_list ap)
+{
+	char *ar = va_arg(ap, char *);
+
+	if (ar == NULL)
+	{
+		printf("%s%s", separator, "(nil)");
+		return;
+	}
+
+	printf("%s%s", separator, ar);
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,20 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+void print_char(char *separator, va_list ap);
+void print_int(char *separator, va_list ap);
+void print_float(char *separator, va_list ap);
+void print_char_ptr(char *separator, va_list ap);
+
+typedef struct format_types
+{
+	char *type;
+	void (*f)(char *separator, va_list ap);
+} f_dt;
 #endif /* _HOLBERTON_H_ */

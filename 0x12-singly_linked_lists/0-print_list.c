5e8f2fcfec16726092f1a54e33e328c1a706ea30
evertcolombia <evertescalante@gmail.com>
2019-11-02 17:12:27 +0000

    hotfix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,12 +15,13 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 
-		if (h->str != NULL)
-			printf("[%d] %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 
-		h = h->next, tmp++;
+		h = h->next;
+		tmp++;
 	}
 	return (tmp);
 }

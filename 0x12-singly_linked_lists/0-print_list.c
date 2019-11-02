0ff56fbfe5f7b36b880e5c7b5ff401872b6c6943
evertcolombia <evertescalante@gmail.com>
2019-11-02 16:24:25 +0000

    hotfix

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_list - print a linked list
+ * @h:	linked list
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	unsigned int tmp;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		tmp++;
+		}
+	}
+	return (tmp);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+#This directory contains some exercises with Linked List in C

791949364b99c7aed9b04732e1082c37365d077f
evertcolombia <evertescalante@gmail.com>
2019-11-04 00:30:52 +0000

    hotfix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,7 +11,7 @@ char *_strstr(char *haystack, char *needle)
 		if (needle == '\0')
 			return (haystack);
 
-		while (*haystack)
+		while (*haystack != '\0')
 		{
 			if (*haystack == *needle)
 				return (haystack);

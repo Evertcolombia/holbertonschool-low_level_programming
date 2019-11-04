5d08f4ff0cd0ef2e4cac881720dee10d9e6fd505
evertcolombia <evertescalante@gmail.com>
2019-11-04 06:47:53 +0000

    hotfix

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - changes money
+ * @argc: count arg
+ * @argv: array arg
+ * Return: Sucess 0, Error 1
+ */
+int main(int argc, char *argv[])
+{
+	int c[5] = {25, 10, 5, 2, 1};
+	int sum = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	int num = atoi(argv[1]);
+
+	if (num < 0)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	while (num > 0)
+	{
+		if ((num - c[0]) >= 0)
+			num = num - c[0], sum++;
+		else if ((num - c[1]) >= 0)
+			num = num - c[1], sum++;
+		else if ((num - c[2]) >= 0)
+			num = num - c[2], sum++;
+		else if ((num - c[3]) >= 0)
+			num = num - c[3], sum++;
+		else if (num >= c[4])
+		num = num - c[4], sum++;
+	}
+	printf("%d\n", sum);
+	return (0);
+}

763fe0daa33934d9174af12df9fa12268c177fcf
evertcolombia <evertescalante@gmail.com>
2019-11-05 15:18:12 +0000

    hotfix

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - calculate the len oh a list
+ * @h: linked list
+ *
+ * Return: node counter
+ */
+size_t listint_len(const listint_t *h)
+{
+	float count;	
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	} return (count);
+}

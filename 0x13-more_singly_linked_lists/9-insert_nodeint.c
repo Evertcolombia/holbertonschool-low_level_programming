cd1cbc1c60b004078886ea1467f2b860252b4011
evertcolombia <evertescalante@gmail.com>
2019-11-05 20:47:50 +0000

    hotfix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,6 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
+	new_node = new_node->next;
 
 	while (tmp != NULL)
 	{

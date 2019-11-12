60a531d18de65d91ad9ebea51476b50cb767f2c1
evertcolombia <evertescalante@gmail.com>
2019-11-12 20:26:37 +0000

    function that reads a text file and prints it to the POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "holberton.h"
+
+size_t read_textfile(const char *filename, size_t letters)
+{
+int fd, _strlen;
+char *buffer;
+    /*if pointer to file is empty*/
+if (filename == NULL)
+return (0);
+
+/*open and read file*/
+    fd = open(filename, O_RDONLY);
+    if (fd == -1)
+        return (0);
+
+    /*memory space for the buffer to prduce*/
+    buffer = malloc(sizeof(char) * letters);
+    if (buffer == NULL)
+        return (0);
+
+    /*read(file, start, numbert to read)*/
+    _strlen = read(fd, buffer, letters);
+
+    /*if the stdout  != to the len of the buffer return 0*/
+    if (write(STDOUT_FILENO, buffer, _strlen) != _strlen)
+        return (0);
+    /*close file descriptor*/
+    close(fd);
+    /*free memory*/
+    free(buffer);
+    /*return len*/
+    return (_strlen);
+}
---
+++ 0x15-file_io/holberton.h
@@ -0,0 +1,4 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+size_t read_textfile(const char *filename, size_t letters);
+#endif /* _HOLBERTON_H_ */

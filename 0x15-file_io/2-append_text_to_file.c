df35702ccfbbc77139b09d61bd3fa111270a8495
evertcolombia <evertescalante@gmail.com>
2019-11-12 22:42:32 +0000

    unction that appends text at the end of a file.

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,52 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
+
+int _strlen(char *s);
+
+/**
+ * append_text_to_file - append text in a file
+ * @filename: pointer to file
+ * @text_content: pointer to string
+ *
+ * Return: 1 on success
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, len;
+
+	len = _strlen(text_content);
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content == NULL)
+		return (1);
+
+	if (write(fd, text_content, len) != len)
+		return (-1);
+
+	return (1);
+}
+
+/**
+ * _strlen - show the lenght of a string
+ * @s: pointer to  pass the array
+ *
+ * Return: none
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	while (s[a])
+		++a;
+
+	return (a);
+}

e71cb00c84d83a11ad3dc330e209b0d26c58b200
evertcolombia <evertescalante@gmail.com>
2019-12-02 01:34:02 +0000

    hotfix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,9 +8,10 @@
 int main(void)
 {
 	unsigned long num_1 = 0, num_2 = 1, sum = 0, total = 0;
+	unsigned long limit = 4000000;
 	int count;
 
-	for (count = 0; count < 32; count++)
+	while (sum < limit)
 	{
 		sum = num_1 + num_2;
 		num_1 = num_2;
@@ -18,8 +19,6 @@ int main(void)
 
 		if (sum % 2 == 0)
 			total += sum;
-
-
 	}
 	printf("%lu", total);
 	putchar(10);

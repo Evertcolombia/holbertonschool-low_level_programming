cff0e31d59cf3fbbf31c8b7add1cdaba61d03dcf
evertcolombia <evertescalante@gmail.com>
2019-12-14 22:58:33 +0000

    3-hotfix

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -26,7 +26,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = new_node;
-		return (new_node);
+		return (head);
 	}
 	while (tmp->next)
 		tmp = tmp->next;
@@ -34,5 +34,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	tmp = new_node;
 	new_node->prev = tmp;
 
-	return (new_node);
+	return (head);
 }

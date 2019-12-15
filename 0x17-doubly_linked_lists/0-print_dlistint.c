a5cf579eb46fe15ea16089f71b5b72a09c15348b
evertcolombia <evertescalante@gmail.com>
2019-12-15 01:15:34 +0000

    0-hotfix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,17 +7,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	const dlistint_t *tmp;
 	size_t count = 0;
 
-	if (h ==  NULL)
-		return (-1);
-
-	tmp = h;
-	while (tmp)
+	while (h)
 	{
 		printf("%d\n", tmp->n);
 		count++;
-		tmp = tmp->next;
+		h = h->next;
 	} return (count);
 }

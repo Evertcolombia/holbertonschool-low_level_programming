2082285520608d82010b4e9e551010ab2d771842
evertcolombia <evertescalante@gmail.com>
2020-01-18 21:53:25 +0000

    hotfix

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,13 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	if (ht == NULL || !key)
 		return (NULL);
-
 	index = key_index((unsigned char *)key, ht->size);
 
 	while (ht->array[index])
 	{
 		if (strcmp(key, ht->array[index]->key) == 0)
 			return (ht->array[index]->value);
-		ht->array[index] = ht->array[index]->next;	
+		ht->array[index] = ht->array[index]->next;
 	} return (NULL);
 }

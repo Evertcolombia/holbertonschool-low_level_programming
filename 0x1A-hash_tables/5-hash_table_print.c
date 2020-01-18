62390657e620c6813cdfc1ceedc69ff7613dcad0
evertcolombia <evertescalante@gmail.com>
2020-01-18 23:58:53 +0000

    hotfix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,7 +7,7 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned int i, flat = 0;
+	unsigned int i= 0, flat = 0;
 	hash_node_t *tmp = NULL;
 
 	if (ht == NULL)
@@ -18,13 +18,14 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[i] == NULL)
 			continue;
-
-		for (tmp = ht->array[i]; tmp != NULL; tmp = tmp->next)
+		tmp = ht->array[i];
+		while (tmp != NULL)
 		{
 			if (flat > 0)
 				printf(", ");
 			printf("'%s': '%s'", tmp->key, tmp->value);
 			flat++;
+			tmp = tmp->next;
 		}
 	}
 	printf("}\n");

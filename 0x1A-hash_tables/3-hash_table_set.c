c7681365aa0fdeac1423074ba0a0e9fa783c05da
evertcolombia <evertescalante@gmail.com>
2020-01-18 19:25:18 +0000

    hotfix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,8 +8,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL)
 		return(0);
 
-	if (key == NULL)
-		return(0);
+	/*if (key == NULL)
+		return(0);*/
 
 	if (value == NULL)
 		return (0);

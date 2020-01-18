33b6c73d88aaeb01db903b63241feadd5d6d77cd
evertcolombia <evertescalante@gmail.com>
2020-01-18 23:18:38 +0000

    hotfix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -3,26 +3,22 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i = 0;
+	hash_node_t *tmp = NULL;
 
 	if (ht == NULL)
 		printf("{}\n");
 
-	while (ht->array[i])
+	printf("{");
+	while (i <= ht->size)
 	{
-		printf("pasa\n");
 		if (ht->array[i] != NULL)
 		{
-			if (ht->array[i]->next != NULL)
-			{
-				while(ht->array[i]-> next != NULL)
-					printf("'%s':  '%s' ", ht->array[i]->key, ht->array[i]->value);
-					ht->array[i] = ht->array[i]->next;
-			}
+			tmp = ht->array[i];
+			if (tmp->next != NULL)
+				printf("'%s': '%s', ", tmp->next->key, tmp->next->value);
 			else
-			{
-				printf("'%s':  '%s' ", ht->array[i]->key, ht->array[i]->value);
-			}
-		}
-		i++;
-	}
+				printf("'%s':  '%s' ", tmp->key, tmp->value);
+		} i++;
+	} printf("}\n");
 }
+

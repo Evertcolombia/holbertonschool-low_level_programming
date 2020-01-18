d2e018f7a556771483dc4cf9ba8311c02d1df4b3
evertcolombia <evertescalante@gmail.com>
2020-01-18 21:41:53 +0000

    4-function that retrieves a vualue in a specific key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value in a specific key
+ * @ht: hash table
+ * @key: key to hashing
+ *
+ * Return: value on success null on fail
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+
+	if (ht == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	if (ht->array[index] != NULL)
+	{
+		if (strcmp(key, ht->array[index]->key) == 0)
+		{
+			return (ht->array[index]->value);
+		}
+	}
+
+	return (NULL);
+}

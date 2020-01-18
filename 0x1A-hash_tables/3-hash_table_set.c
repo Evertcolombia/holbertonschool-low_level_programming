7d45bddb809e07a6c33a86a72530443d4ea88b33
evertcolombia <evertescalante@gmail.com>
2020-01-18 19:28:42 +0000

    hotfix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,8 +11,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	/*if (key == NULL)
 		return(0);*/
 
-	if (value == NULL)
-		return (0);
+	/*if (value == NULL)
+		return (0);*/
 
 	index =  key_index((unsigned char *)key, ht->size);
 

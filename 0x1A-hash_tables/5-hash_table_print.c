6e526ffd5bfe1cf6b8fbcb309f52bfe28c00a27b
evertcolombia <evertescalante@gmail.com>
2020-01-18 23:49:40 +0000

    hotfix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,8 +1,13 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_print - print a hash table
+ * @ht: hhash table
+ *
+ * Return: none
+ */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned  int i, state = 0;
+	unsigned int i, flat = 0;
 	hash_node_t *tmp = NULL;
 
 	if (ht == NULL)
@@ -13,14 +18,14 @@ void hash_table_print(const hash_table_t *ht)
 	{
 		if (ht->array[i] == NULL)
 			continue;
+
 		for (tmp = ht->array[i]; tmp != NULL; tmp = tmp->next)
 		{
-			if (state > 0)
+			if (flat > 0)
 				printf(", ");
-			printf("'%s': '%s', ", tmp->key, tmp->value);
-			state++;
+			printf("'%s': '%s'", tmp->key, tmp->value);
+			flat++;
 		}
 	}
 	printf("}\n");
 }
-

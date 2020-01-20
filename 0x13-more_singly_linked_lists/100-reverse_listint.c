2999b65a842a73d2cd3b1c13484f873a1adb677f
evertcolombia <evertescalante@gmail.com>
2020-01-20 01:37:10 +0000

    hotfix

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - reverse a lined list
+ * @head: head of list
+ *
+ * Return: head of list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *tmp = NULL, *tmp1 = NULL;;
+
+	if (*head != NULL)
+	{
+		tmp = *head;
+		tmp1 = (*head)->next;
+		*head = (*head)->next;
+
+		tmp->next = NULL;
+
+		while (*head != NULL)
+		{
+			*head = (*head)->next;
+			tmp1->next = tmp;
+
+			tmp = tmp1;
+			tmp1 = *head;
+		}
+		*head = tmp;
+	} return (*head);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LIST_H_
 #define _LIST_H_
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -27,5 +29,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
 
 #endif /* _LIST_H_ */

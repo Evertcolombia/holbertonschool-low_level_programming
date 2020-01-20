662b4ba678b85805c37cc3f1f9546a10eff8a015
evertcolombia <evertescalante@gmail.com>
2020-01-20 02:03:25 +0000

    hotfix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,15 +10,15 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 
-	while (head)
+	while (head->next != NULL)
 	{
-		if ((head->next) >= head)
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		if ((head->next) > head)
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
 			return (count);
 		}
-		printf("[%p] %d\n", (void *)head, head->n);
-		count++;
 		head = head->next;
 	} return (count);
 }

832ae75c995db2290f110ddaece914ca3e1db8c0
evertcolombia <evertescalante@gmail.com>
2020-01-20 01:44:04 +0000

    101-function

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,5 +2,16 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
+	size_t count = 0;
 
+	if (head == NULL)
+		exit(98);
+
+	while (head != NULL)
+	{
+		printf("[] %i\n", head->n);
+		count++;
+		head = head->next;
+	}
+	return (count);
 }

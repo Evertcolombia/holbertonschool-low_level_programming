16eb13a0d53a2bb4398598a05562cc84375ee329
evertcolombia <evertescalante@gmail.com>
2020-01-22 07:22:33 +0000

    hotfix

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -63,6 +63,10 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value){
 
 	if ((s_list = malloc(sizeof(shash_node_t))) == NULL)
                         return (0);
+	s_list->key = strdup(key);
+	s_list->key = strdup(value);
+	s_list->next = ht->array[id];
+	ht->array[id] = s_list;
 
 	if (!(ht->shead))
 	{
@@ -71,15 +75,28 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value){
 	}
 	else
 	{
-		tmp = ht->head
+		tmp = ht->shead;
+		while (tmp)
+		{
+			if (strcmp(s_list->key, tmp->key) <= 0)
+			{
+				s_list->snext = tmp;
+				s_list->sprev = tmp->sprev;
 
-		s_list->key = strdup(key);
-		s_list->value = strdup(value);
-		s_list->next = ht->array[id];
-		s_list->sprev = NULL;
-		s_list->snext = NULL;
+				if (tmp->sprev)
+					tmp->sprev->snext = s_list;
+				else
+					ht->shead = s_list;
+				tmp->sprev = s_list;
+				break;
+			}
+		}
 
-		ht->array[id] = s_list;
-
-	return (1);
+		if (!tmp)
+		{
+			ht->stail->next = s_list;
+			s_list->sprev = ht->stail;
+			ht->stail = s_list;
+		}
+	} return (1);
 }

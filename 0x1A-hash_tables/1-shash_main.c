77ba3aa54e8ba961c834d2b9a498b51b802abe5e
evertcolombia <evertescalante@gmail.com>
2020-01-22 20:56:19 +0000

    improving function

--- 0x1A-hash_tables/1-shash_main.c
+++ 0x1A-hash_tables/1-shash_main.c
@@ -13,6 +13,25 @@ int main(void)
     shash_table_t *ht;
 
     ht = shash_table_create(1024);
+	printf("Table was created\n");
     shash_table_set(ht, "betty", "holberton");
+    shash_table_set(ht, "hetairas", "holberton");
+    shash_table_set(ht, "mentioner", "holberton");
+    shash_table_set(ht, "heliotropes", "neurospora");
+    shash_table_set(ht, "depravement", "serafins");
+    shash_table_set(ht, "stylist", "holberton");
+    shash_table_set(ht, "joyful", "holberton");
+    shash_table_set(ht, "synaphea", "holberton");
+    shash_table_set(ht, "redescribed", "urites");
+    shash_table_set(ht, "dram", "holberton");
+    shash_table_set(ht, "hetairas", "holberton");
+    shash_table_set(ht, "mentioner", "holberton");
+    shash_table_set(ht, "heliotropes", "neurospora");
+    shash_table_set(ht, "depravement", "serafins");
+    shash_table_set(ht, "stylist", "holberton");
+    shash_table_set(ht, "joyful", "holberton");
+    shash_table_set(ht, "synaphea", "holberton");
+    shash_table_set(ht, "redescribed", "urites");
+    shash_table_set(ht, "dram", "holberton");
     return (EXIT_SUCCESS);
 }
--- 0x1A-hash_tables/11.c
+++ 0x1A-hash_tables/11.c
@@ -35,7 +35,6 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value){
 		return (0);
 
 	id = key_index((unsigned char *) key, ht->size);
-
 	if (ht->array[id] != NULL)
 	{
 		while (ht->array[id])
@@ -44,6 +43,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value){
                 	{
                         	free(ht->array[id]->value);
 	                        ht->array[id]->value = strdup(value);
+				printf("Remplazo value\n\n");
         	                return (1);
                		}
 			ht->array[id] = ht->array[id]->next;
@@ -57,33 +57,44 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value){
                 s_list->sprev = NULL;
                 s_list->snext = NULL;
 		ht->array[id] = s_list;
+		printf("no hubo llave igual, creamos nuevo nodo al comienzo de la lista\n\n");
 		return (1);
 	}
-
+	printf("No hay nada en ese id\n");
 	if ((s_list = malloc(sizeof(shash_node_t))) == NULL)
                         return (0);
 	s_list->key = strdup(key);
 	s_list->key = strdup(value);
 	s_list->next = ht->array[id];
 	ht->array[id] = s_list;
+	printf("Node creado y enlazado al array\n");
 
 	if (ht->shead)
 	{
+		printf("ya hay nodos en la lista ordenada\n");
 		tmp = ht->shead;
 		while (tmp)
 		{
+			printf("recorriendo lista ordenada\n");
 			if (strcmp(s_list->key, tmp->key) <= 0)
 			{
 				s_list->snext = tmp;
 				s_list->sprev = tmp->sprev;
-
+				printf("hay una llave que es igual a la key  en el nodo de turno\n");
 				if (tmp->sprev)
+				{
 					tmp->sprev->snext = s_list;
+					printf("hay  un nodo antes de el tmp de turno, ste nodo ahora apunta a s_list\n\n");
+				}
 				else
+				{
+					printf("else\n");
 					ht->shead = s_list;
+				}
 				tmp->sprev = s_list;
 				break;
 			}
+			tmp = tmp->snext;
 		}
 
 		if (!tmp)
@@ -95,6 +106,7 @@ int shash_table_set(shash_table_t *ht, const char *key, const char *value){
 	}
 	else
 	{
+		printf("shead y stail apuntant a s_list\n\n");
 		ht->shead = s_list;
 		ht->stail = s_list;
 	} return (1);

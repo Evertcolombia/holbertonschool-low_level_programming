95ccf18640f3cb3269baaa37cb8cce1d047af389
evertcolombia <evertescalante@gmail.com>
2020-01-22 05:17:01 +0000

    1-create_hash_table_sorted function

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -2,5 +2,21 @@
 
 shash_table_t *shash_table_create(unsigned long int size)
 {
+	shash_table_t *table = NULL;
+	shash_node_s *new_node = NULL;
 
+	if (size < 1)
+		return(NULL);
+
+	table = malloc(sizeof(shash_table_t ));
+	if (table == NULL)
+		return (NULL);
+
+	table->array = calloc(size, sizeof(shash_node_s *));
+	if (new_node == NULL)
+		return (NULL);
+
+	table->size = size;
+	table->shead = NULL;
+	table->stail = NULL;
 }

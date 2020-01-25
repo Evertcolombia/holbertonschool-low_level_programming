6ab80e7ec8ec5ec999fa69a23d19e9cc9dbc240c
evertcolombia <evertescalante@gmail.com>
2020-01-25 06:16:46 +0000

    hotfix

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -3,7 +3,7 @@
 shash_table_t *shash_table_create(unsigned long int size)
 {
 	shash_table_t *table = NULL;
-	shash_node_t **array = NULL;
+	/*shash_node_t **array = NULL;*/
 
 	if (size < 1)
 		return(NULL);
@@ -11,15 +11,15 @@ shash_table_t *shash_table_create(unsigned long int size)
 	if ((table = malloc(sizeof(shash_table_t ))) == NULL)
 		return (NULL);
 
-	if((array = calloc(size, sizeof(shash_node_t *))) == NULL)
-        /*if((table->array = calloc(size, sizeof(shash_node_t *))) == NULL)*/
+	/*if((array = calloc(size, sizeof(shash_node_t *))) == NULL)*/
+        if((table->array = calloc(size, sizeof(shash_node_t *))) == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
 
 	table->size = size;
-	table->array = array;
+	/*table->array = array;*/
 	table->shead = NULL;
 	table->stail = NULL;
 

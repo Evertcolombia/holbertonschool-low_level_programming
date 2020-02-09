1c771e6997bc477c4824f5e66bde80afaaff2192
evertcolombia <evertescalante@gmail.com>
2020-02-09 23:10:00 +0000

    hotfix

--- 0x06-pointers_arrays_strings/6-main.c
+++ 0x06-pointers_arrays_strings/6-main.c
@@ -9,8 +9,9 @@
 int main(void)
 {
    /* char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";*/
-	char s[] = "hello world! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
+	/*char s[] = "hello world! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";*/
     char *p;
+	char s [] = "HELLO WORLD! expect the best. Prepare for the worst. Capitalize on what comes.\nhello world! hello-world 0123456hello world\thello world.hello world\n";
 
     p = cap_string(s);
     printf("%s", p);

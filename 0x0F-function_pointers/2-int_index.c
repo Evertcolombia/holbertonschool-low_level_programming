cf4b6b04fab42d9787cdff85949fbe6560deb0da
evertcolombia <evertescalante@gmail.com>
2021-01-13 00:48:54 +0000

    function pointers hotfix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-/**
+  /**
  * int_index - search for an element
  * @array: array integers
  * @size: value
@@ -6,15 +6,18 @@
  *
  * Return: i
  */
+ 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || !array || !cmp)
 		return (-1);
+
 	for (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]) != 0)
+		if (cmp(array[i]))
 			return (i);
 	}
+	return (-1);
 }
